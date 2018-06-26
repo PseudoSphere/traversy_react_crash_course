493cbdfc561e3d4293d46d1cdffb7986046015bd
Caleb Ferrell <Calebjf@gmail.com>
2018-06-25 19:20:21 -0700

    components!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {projects: []}
+  }
+  
+  componentDidMount() {
+    this.setState({
+      projects: [
+      {
+        title: 'Business Website',
+        category: 'Web Design'
+      },
+      {
+        title: 'Social App',
+        category: 'Mobile Dev'
+      },
+      {
+        title: 'Shopping Cart',
+        category: 'Web Development'
+      }
+    ]});
+  }
+
   render() {
     return (
       <div className="App">
         My App
+        <Projects projects = {this.state.projects}/>
       </div>
     );
   }
