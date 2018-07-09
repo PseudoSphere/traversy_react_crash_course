77da678dd58d5f55dd849814bef5b7b3e2f74743
Caleb Ferrell <Calebjf@gmail.com>
2018-07-09 16:55:14 -0700

    Added comments as form of review and for benefit of future me. You're welcome future me.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+// Required Import
 import React, { Component } from 'react';
+// Generate unique ids
 import uuid from 'uuid';
+
 import $ from 'jquery';
-import Projects from './Components/Projects';
+// Importing named exports. Groovy! (Not actually using ProjectItem; only imported to test compiling)
+import {Projects, ProjectItem} from './Components/Projects';
+// Import component from another file
 import AddProject from './Components/AddProject';
+// Required Import
 import './App.css';
 
+// Required class
 class App extends Component {
+  // Instantiate state object
   constructor() {
     super();
     this.state = {
@@ -14,6 +22,7 @@ class App extends Component {
     }
   }
   
+  // Unused but proof of concept
   getTodos() {
     $.ajax({
       url: 'https://jsonplaceholder.typicode.com/todos',
@@ -30,6 +39,7 @@ class App extends Component {
     })
   }
 
+  // Generate project data
   getProjects() {
     this.setState({
       projects: [
@@ -51,24 +61,38 @@ class App extends Component {
     ]});
   }
 
-  componentDidMount() {
-    // This is the wrong place to fetch data
-    //this.getProjects();
-    //this.getTodos();
-  }
-
+  // Called before render()
   componentWillMount() {
     // This is the RIGHT place to fetch data
     this.getProjects();
     this.getTodos();
   }
 
+  // Called after render()
+  componentDidMount() {
+
+  }
+
+  // Adds a project to the list
   handleAddProject(project) {
     let state = this.state;
     state.projects.push(project);
     this.setState(state);
   }
+  
+  // Top of the "deleteProject" chain
+  /* 
+        TLDR:
+        (ProjectItem)OnClick -> (ProjectItem)deleteProject -> (ProjectItem)onDelete = (Projects)deleteProject -> (Projects)onDelete = (App)handleDeleteProject;
 
+        Verbose:
+        (ProjectItem)OnClick triggers (ProjectItem)deleteProject
+        (ProjectItem)deleteProject calls (ProjectItem)onDelete
+        (ProjectItem)onDelete equals (Projects)deleteProject
+        (Projects)deleteProject calls (Projects)onDelete
+        (Projects)onDelete equals (App)handleDeleteProject
+        (App)handleDeleteProject updates the state which updates the DOM
+    */
   handleDeleteProject(id) {
     let state = this.state;
     let index = state.projects.findIndex(x => x.id === id);
@@ -76,9 +100,12 @@ class App extends Component {
     this.setState(state);
   }
 
+  // Must be a pure function (Always returns the same result)
   render() {
     return (
+      // Single element return (required)
       <div className="App">
+        {/* My two main components */}
         <AddProject addProject={this.handleAddProject.bind(this)}/>
         <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
       </div>
@@ -86,4 +113,5 @@ class App extends Component {
   }
 }
 
+// required export
 export default App;
